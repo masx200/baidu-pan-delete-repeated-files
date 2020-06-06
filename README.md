23bf589638b2215713d799b778e5ae24fb7bc555
masx200 <masx200@qq.com>
2020-06-06 20:43:18 +0800

    修复bug

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 删除百度网盘中重复的文件,数据从 mongodb 中获取.这个代码库是`百度网盘批量清理重复文件计划`的一部分。
 
+数据库名称:"baidupan"
+
+集合名称: "panfile"
+
 百度网盘批量清理重复文件计划
 
 https://github.com/masx200/baidu-pan-delete-repeated-files
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,21 @@
-import { deletefiles, initPANENV } from "@masx200/fetch-baidu-pan-files";
+import {
+    deletefiles /* , initPANENV  */,
+} from "@masx200/fetch-baidu-pan-files";
 import findrepeat from "@masx200/mongodb-file-find-md5-repeat";
 import fsextra from "fs-extra";
 async function start() {
-    const panenv = await initPANENV();
-    const jsonfile = await findrepeat("pan_" + panenv.user, "panfile");
+    // const panenv = await initPANENV();
+    const jsonfile = await findrepeat(
+        "baidupan",
+        /* "pan_" + panenv.user, */ "panfile"
+    );
     const dataarray = await fsextra.readJSON(jsonfile);
     if (!Array.isArray(dataarray)) {
         throw TypeError("invalid data array");
     }
     const values = Object.values(Object.fromEntries(dataarray));
     const filestodelete = values
-        .map(paths => {
+        .map((paths) => {
             if (!Array.isArray(paths)) {
                 throw TypeError("invalid data array");
             }
